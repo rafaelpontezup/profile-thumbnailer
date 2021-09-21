9f65ba5ba4d489285a32751603a90b81e7c631a5
Rafael Ponte <rponte@gmail.com>
2021-09-21 16:28:19 -0300

    refatora código para dividir funcionalidades em 2 controllers;

--- src/main/java/br/com/zup/edu/thumbnailer/profile/CreateThumbnailController.java
+++ src/main/java/br/com/zup/edu/thumbnailer/profile/CreateThumbnailController.java
@@ -3,20 +3,19 @@ package br.com.zup.edu.thumbnailer.profile;
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.geometry.Positions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.UUID;
 
 @RestController
-public class ProfileThumbnailerController {
+public class CreateThumbnailController {
 
     @Autowired
     private Watermark watermark;
@@ -33,28 +32,6 @@ public class ProfileThumbnailerController {
 
         ProfilePhotoResponse response = ProfilePhotoResponse.of(photo.getFileName(), thumbnail);
         return ResponseEntity.ok(response);
-    }
-
-    @PostMapping(
-        value = "/api/users/{id}/thumbnails/preview",
-        produces = MediaType.IMAGE_PNG_VALUE
-    )
-    public @ResponseBody byte[] previewThumbnail(@PathVariable UUID id, @Valid @RequestBody ProfilePhotoRequest photo) {
-
-        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-
-            BufferedImage originalImage = photo.toImage();
-            Thumbnails.of(originalImage)
-                    .size(200, 200)
-                    .outputFormat("png")
-                    .watermark(Positions.BOTTOM_RIGHT, watermark.getWatermarkImage(), 0.5f)
-                    .toOutputStream(output);
-
-            return output.toByteArray();
-
-        } catch (IOException e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-        }
     }
 
 }
